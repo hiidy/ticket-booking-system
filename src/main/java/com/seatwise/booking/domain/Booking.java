c28a5d35298d7eaf51dcd5c6484a204fb9200f65
hiidy <hiidy115@gmail.com>
2025-01-25 22:53:00 +0900

    refactor: user_id 칼럼명을 member_id로 변경

--- src/main/java/com/seatwise/booking/domain/Booking.java
+++ src/main/java/com/seatwise/booking/domain/Booking.java
@@ -29,7 +29,7 @@ public class Booking extends BaseEntity {
   private Long id;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id")
+  @JoinColumn(name = "member_id")
   private Member member;
 
   @OneToMany(mappedBy = "booking")
