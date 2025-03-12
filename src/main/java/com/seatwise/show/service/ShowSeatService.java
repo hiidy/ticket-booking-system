75320da9c2c4fb9f88acd71b4523c0fb25603584
hiidy <hiidy115@gmail.com>
2025-03-12 18:53:18 +0900

    refactor: ShowSeatService에서 클래스 레벨 @Transactional 제거

--- src/main/java/com/seatwise/show/service/ShowSeatService.java
+++ src/main/java/com/seatwise/show/service/ShowSeatService.java
@@ -14,11 +14,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 @Slf4j
 public class ShowSeatService {
 
