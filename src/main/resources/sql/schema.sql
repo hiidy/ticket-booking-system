243f4d5bcd422376d39e082e2e3d6e07841bcead
hiidy <hiidy115@gmail.com>
2025-04-14 23:33:44 +0900

    feat: 모든 ID 타입을 INT에서 BIGINT로 변경

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,20 +1,20 @@
-DROP DATABASE booking_system;
+DROP DATABASE IF EXISTS booking_system;
 CREATE DATABASE booking_system;
 USE booking_system;
 
 CREATE TABLE seat
 (
-    id          INT AUTO_INCREMENT PRIMARY KEY,
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
     seat_number INT         NOT NULL,
     grade       VARCHAR(50) NOT NULL,
-    venue_id    INT         NOT NULL,
+    venue_id    BIGINT      NOT NULL,
     created_at  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE member
 (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     name       VARCHAR(50)  NOT NULL,
     email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE member
 
 CREATE TABLE event
 (
-    id          INT AUTO_INCREMENT PRIMARY KEY,
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
     title       VARCHAR(100) NOT NULL,
     description TEXT         NOT NULL,
     type        VARCHAR(50)  NOT NULL,
@@ -34,8 +34,8 @@ CREATE TABLE event
 
 CREATE TABLE `show`
 (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    event_id   INT       NOT NULL,
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    event_id   BIGINT    NOT NULL,
     date       DATE      NOT NULL,
     start_time TIME      NOT NULL,
     end_time   TIME      NOT NULL,
@@ -45,8 +45,8 @@ CREATE TABLE `show`
 
 CREATE TABLE booking
 (
-    id           INT AUTO_INCREMENT PRIMARY KEY,
-    member_id    INT       NOT NULL,
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id    BIGINT    NOT NULL,
     total_amount BIGINT    NOT NULL,
     timestamp    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -55,10 +55,10 @@ CREATE TABLE booking
 
 CREATE TABLE show_seat
 (
-    id              INT AUTO_INCREMENT PRIMARY KEY,
-    show_id         INT          NOT NULL,
-    seat_id         INT          NOT NULL,
-    booking_id      INT,
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    show_id         BIGINT       NOT NULL,
+    seat_id         BIGINT       NOT NULL,
+    booking_id      BIGINT,
     price           BIGINT       NOT NULL,
     status          VARCHAR(100) NOT NULL,
     expiration_time TIMESTAMP,
@@ -68,7 +68,7 @@ CREATE TABLE show_seat
 
 CREATE TABLE venue
 (
-    id          INT AUTO_INCREMENT PRIMARY KEY,
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
     name        VARCHAR(100),
     total_seats INT       NOT NULL,
     created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -77,7 +77,7 @@ CREATE TABLE venue
 
 CREATE TABLE inventory
 (
-    show_id         INT         NOT NULL,
+    show_id         BIGINT      NOT NULL,
     grade           VARCHAR(50) NOT NULL,
     total_count     INT         NOT NULL,
     available_count INT         NOT NULL,
