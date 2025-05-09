750507832d5ee0dd4d87ffc2c10591da8da50103
hiidy <hiidy115@gmail.com>
2025-05-09 23:09:55 +0900

    refactor: EventRepository에서 불필요한 @Repository 어노테이션 제거

--- src/main/java/com/seatwise/event/repository/EventRepository.java
+++ src/main/java/com/seatwise/event/repository/EventRepository.java
@@ -5,9 +5,7 @@ import com.seatwise.event.domain.EventType;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
   List<Event> findAllByType(EventType type, Pageable pageable);
