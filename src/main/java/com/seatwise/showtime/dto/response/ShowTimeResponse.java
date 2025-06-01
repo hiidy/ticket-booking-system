db9f940702d08439117080c7f3b1395e9947c1dd
hiidy <hiidy115@gmail.com>
2025-06-01 22:54:46 +0900

    refactor(dto): 사용되지 않는 응답 DTO 클래스 제거

--- src/main/java/com/seatwise/showtime/dto/response/ShowTimeResponse.java
+++
@@ -1,6 +0,0 @@
-package com.seatwise.showtime.dto.response;
-
-import java.time.LocalTime;
-import java.util.List;
-
-public record ShowTimeResponse(LocalTime startTime, List<SeatRemainingResponse> remainingSeats) {}
